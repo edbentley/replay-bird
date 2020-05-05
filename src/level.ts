cd8844b89af7fdb3e31657ef99a65dbee17da836
Ed Bentley <15923595+edbentley@users.noreply.github.com>
2020-05-05 15:59:01 +0100

    part14

--- src/level.ts
+++ src/level.ts
@@ -1,9 +1,11 @@
-import { makeSprite, t } from "@replay/core";
+import { makeSprite, t, Device } from "@replay/core";
 import { WebInputs } from "@replay/web";
 import { iOSInputs } from "@replay/swift";
-import { Bird } from "./bird";
+import { Bird, birdWidth } from "./bird";
 import { isWebInput } from "./utils";
+import { Pipe, PipeT, pipeWidth, pipeGap } from "./pipe";
 
+const speedX = 2;
 const birdX = 0;
 
 type LevelProps = {
@@ -13,13 +15,15 @@ type LevelProps = {
 type LevelState = {
   birdY: number;
   birdGravity: number;
+  pipes: PipeT[];
 };
 
 export const Level = makeSprite<LevelProps, LevelState, WebInputs | iOSInputs>({
-  init() {
+  init({ device }) {
     return {
       birdY: 10,
       birdGravity: -12,
+      pipes: [newPipe(device)],
     };
   },
 
@@ -30,7 +34,7 @@ export const Level = makeSprite<LevelProps, LevelState, WebInputs | iOSInputs>({
 
     const { inputs } = device;
 
-    let { birdGravity, birdY } = state;
+    let { birdGravity, birdY, pipes } = state;
 
     birdGravity += 0.8;
     birdY -= birdGravity;
@@ -42,9 +46,30 @@ export const Level = makeSprite<LevelProps, LevelState, WebInputs | iOSInputs>({
       birdGravity = -12;
     }
 
+    const lastPipe = pipes[pipes.length - 1];
+    if (lastPipe.x < 140) {
+      pipes = [...pipes, newPipe(device)]
+        // Remove the pipes off screen on left
+        .filter(
+          (pipe) =>
+            pipe.x > -(device.size.width + device.size.widthMargin + pipeWidth)
+        );
+    }
+
+    // Move pipes to the left
+    pipes = pipes.map((pipe) => {
+      let passed = pipe.passed;
+      if (!passed && pipe.x < birdX - birdWidth / 2 - pipeWidth / 2) {
+        // Mark pipe as having passed bird's x position
+        passed = true;
+      }
+      return { ...pipe, passed, x: pipe.x - speedX };
+    });
+
     return {
       birdGravity,
       birdY,
+      pipes,
     };
   },
 
@@ -64,6 +89,24 @@ export const Level = makeSprite<LevelProps, LevelState, WebInputs | iOSInputs>({
           rotation: Math.max(-30, state.birdGravity * 3 - 30),
         },
       }),
+      ...state.pipes.map((pipe, index) =>
+        Pipe({
+          id: `pipe-${index}`,
+          pipe,
+          position: { x: pipe.x, y: 0 },
+        })
+      ),
     ];
   },
 });
+
+function newPipe(device: Device<{}>): PipeT {
+  const height = device.size.height + device.size.heightMargin * 2;
+  const randomY = (height - pipeGap * 2) * (device.random() - 0.5);
+
+  return {
+    x: device.size.width + device.size.widthMargin + 50,
+    gapY: randomY,
+    passed: false,
+  };
+}
